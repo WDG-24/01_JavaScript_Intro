d4a1e97b5adfd705165c4cd086b69071afd13a85
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-08-29 13:14:12 +0200

    arrays i

---
+++ 05_JS_Arrays_I_Korrekturen/005_slice/script.js
@@ -0,0 +1,30 @@
+// Slicing
+// =======
+
+// #### Objective:
+
+// Learn how to use the `slice` method in JavaScript to extract portions of an array without modifying the original array.
+
+// #### Instructions:
+
+// 1.  You will be given an array.
+// 2.  Use the `slice` method to extract
+// different portions of the array.
+// 3.  Print the original array and the sliced portions
+//  to the console.
+
+// Is the original array affected?
+
+// Extract different portions of the array and print the results
+
+// items from index 2-4 (including 4)
+
+// console.log('array1:', array1);
+
+// items from index 0-5 (including 5)
+
+// console.log('array2:', array2);
+
+// last 2 items
+
+// console.log(array);
